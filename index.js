fe5cdbe58155f1415c32ec651a1dc914376f2429
dmanh6302 <dmanh6302@gmail.com>
2024-04-24 17:05:11 +0700

    remove export app

--- index.js
+++ index.js
@@ -30,5 +30,3 @@ app.listen(PORT, () => {
   console.log('On local: ' + chalk.blue(`127.0.0.1:${PORT}`))
   console.log('On your network: ' + chalk.blue(`${ip.address()}:${PORT}`))
 })
-
-module.exports = app;
