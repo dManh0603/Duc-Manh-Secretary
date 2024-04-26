7e230c29bfc68ef0741da7eb2b4708c8353a1c2c
dmanh6302 <dmanh6302@gmail.com>
2024-04-26 16:41:09 +0700

    attempt to fix log

--- logger.js
+++ logger.js
@@ -9,7 +9,7 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: './logs/combined.log' }) // Change the filename as needed
+    new winston.transports.File({ filename: './combined.log' }) // Change the filename as needed
   ]
 });
 
